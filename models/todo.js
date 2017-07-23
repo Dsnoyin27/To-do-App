2b63009a751d00daf4c77ebfba116278279422ac
Adesina <dsnoyin27@gmail.com>
2017-07-23 23:03:29 +0100

    generated models and migrations

---
+++ models/todo.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Todo = sequelize.define(
+    'Todo',
+    {
+      title: DataTypes.STRING
+    },
+    {
+      classMethods: {
+        associate: models => {
+          // associations can be defined here
+          Todo.hasMany(models.TodoItem, {
+          foreignKey: 'todoId',
+          as: 'todoItems',
+        });
+      }
+    }
+    });
+  return Todo;
+};
